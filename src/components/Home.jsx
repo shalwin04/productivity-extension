ee6700a21bcbbc56fe2eeeda3951722eae73057f
shalwin04 <shalwinsanju.25cs@licet.ac.in>
2024-11-20 23:21:47 +0530

    updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
     return(
         <div className="h-screen flex flex-col justify-center items-center bg-gray-100">
-            <h1 className="text-5xl font-bold text-center font-lora">Productivity</h1>
+            <p className="text-5xl text-center font-lora">Productivity</p>
         </div>
     );
 }
