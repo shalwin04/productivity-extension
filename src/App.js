a44a10a0593397c072edad51c7c839220c4c2f76
shalwin04 <shalwinsanju.25cs@licet.ac.in>
2024-11-21 19:10:00 +0530

    added ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import Home from "./components/Home";
+// import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="artboard phone-5">
-      {/* <div className="flex flex-col items-center align-middle"> */}
-      {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="artboard phone-5 bg-gray-100">
+      <div className="h-screen flex flex-col items-center">
+        <p className="text-5xl text-center align-text-top font-lora">
+          Productivity
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      {/* </div> */}
-      <Home />
+      </div>
     </div>
   );
 }
